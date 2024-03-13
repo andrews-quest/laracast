db08401205f27002eac6bb8922a927e351cb5dab
andrews-quest <true.andrewsquest@gmail.com>
2024-03-13 17:00:03 +0100

    Series 31. Added Laracast's own frontend layout and partially split it into components and partials.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,43 @@
 <x-layout>
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
 
 
-    @foreach ($posts as $post) 
-        
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post -> title; !!}
-                </a>
-            </h1>
 
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
-            </p>
 
-            {{ $post -> excerpt; }}
+        </div>
+    </main>
 
-        </article>
+{{--    @foreach ($posts as $post)--}}
 
-    @endforeach
+{{--        <article>--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{ $post->slug }}">--}}
+{{--                    {!! $post -> title; !!}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
+
+{{--            <p>--}}
+{{--                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>--}}
+{{--            </p>--}}
+
+{{--            {{ $post -> excerpt; }}--}}
+
+{{--        </article>--}}
+
+{{--    @endforeach--}}
 
 </x-layout>
-    
+
