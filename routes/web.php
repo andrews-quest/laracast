77137e9bcc6d1fd85cb46d9571bacb96f1917ad1
andrews-quest <true.andrewsquest@gmail.com>
2024-03-14 11:32:51 +0100

    Series 34. Coded a categories dropdown menu using Alpine.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ Route::get('/', function () {
     });
      
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -31,7 +32,9 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
